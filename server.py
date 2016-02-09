4a497947b6dc1f376d614c0c10f4d459d49712b8
Marisha Schumacher-Hodge <marisha.schumacher.hodge@gmail.com>
2016-02-08 17:02:30 -0800

    Created project skeleton

---
+++ server.py
@@ -0,0 +1,35 @@
+"""Diversitech"""
+
+from jinja2 import StrictUndefined
+
+from flask import Flask, render_template, redirect, request, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+# from model import
+
+app = Flask(__name__)
+
+app.secret_key = "54312"
+
+# StructUndefined allows for Jinja2 to raise an error when there is an undefined variable
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def index():
+    """Homepage."""
+
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the point
+    # that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    # connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run()
