b050d0f8bf1ebb81452e648a1dc10e105e952d26
Marisha Schumacher-Hodge <marisha.schumacher.hodge@gmail.com>
2016-02-11 15:33:36 -0800

    Companies and Categories displayed on Front end

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from jinja2 import StrictUndefined
 from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-# from model import
+from model import connect_to_db, db, Company, Category
 
 app = Flask(__name__)
 
@@ -19,7 +19,29 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    return render_template("home.html")
+    companies = Company.query.filter(Company.name != 'U.S. Population',
+                                     Company.name != 'average from our sample').order_by('name').all()
+
+    us_population = Company.query.filter(Company.name == 'U.S. Population').one()
+
+    average = Company.query.filter(Company.name == 'average from our sample').one()
+
+    print us_population
+
+    return render_template("home.html", companies=companies, us_population=us_population, average=average)
+
+
+@app.route('/company/<int:company_id>')
+def show_company(company_id):
+    """Company page with diversity details."""
+
+    company = Company.query.get(company_id)
+
+    categories_for_company = company.categories
+
+    return render_template("company-page.html", display_company=company, categories=categories_for_company)
+
+
 
 
 if __name__ == "__main__":
@@ -27,7 +49,7 @@ if __name__ == "__main__":
     # that we invoke the DebugToolbarExtension
     app.debug = True
 
-    # connect_to_db(app)
+    connect_to_db(app)
 
     # Use the DebugToolbar
     DebugToolbarExtension(app)
