5ecfda2048ade7388a35ca2f5f589546fd90a054
Marisha Schumacher-Hodge <marisha.schumacher.hodge@gmail.com>
2016-03-26 22:09:26 -0700

    Use Heroku port and open interface

--- application.py
+++ application.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, redirect, request, flash, session, jso
 from flask_debugtoolbar import DebugToolbarExtension
 from model import connect_to_db, db, Company, Category, Review
 from helper import *
+import os
 
 import requests
 
@@ -456,4 +457,6 @@ if __name__ == "__main__":
     # Use the DebugToolbar
     DebugToolbarExtension(app)
 
-    app.run()
+    PORT = int(os.environ.get("PORT", 5000))
+
+    app.run(debug=True, host="0.0.0.0", port=PORT)
